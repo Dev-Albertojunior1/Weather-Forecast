6bf9f49de1a6eb7a004007f472bf7dc8b6c080fa
Dev-Albertojunior1 <albertojunior848423803@gmail.com>
2024-09-27 23:12:44 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+
+
+function App() {
+  return (
+    <div className="App">
+
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/api/OpenWeatherData.js
@@ -0,0 +1,5 @@
+const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
+const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
+const WEATHER_API_KEY = 'e1ad9ffde89ad9a699b33cbe8ff25f54';
+
